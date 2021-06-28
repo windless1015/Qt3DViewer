a414d6873a6fb0b12bd8622b13c9da3534f15dc6
pangkuanxin <351477478@qq.com>
2021-06-28 11:38:43 +0800

    修改界面布局

--- Qt3DViewer.cpp
+++ Qt3DViewer.cpp
@@ -1,20 +1,13 @@
 #include "Qt3DViewer.h"
-#include <qboxlayout.h>
 #include "openmeshApps/MeshViewerWidget.h"
 
 Qt3DViewer::Qt3DViewer(QWidget *parent)
     : QMainWindow(parent)
+    , _meshViewerWidget(new MeshViewerWidget())
 {
     ui.setupUi(this);
 
-    _meshViewerWidget = new MeshViewerWidget(this);
-    _meshViewerWidget->resize(640, 480);
-    _meshViewerWidget->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);//ÆÌÂú²¼¾Ö
-    displayMesh();
-    QVBoxLayout* vbox = new QVBoxLayout();
-
     OpenMesh::IO::Options opt;
-
     // enable most options for now
     opt += OpenMesh::IO::Options::VertexColor;
     opt += OpenMesh::IO::Options::VertexNormal;
@@ -24,11 +17,7 @@ Qt3DViewer::Qt3DViewer(QWidget *parent)
     opt += OpenMesh::IO::Options::FaceTexCoord;
 
     _meshViewerWidget->setOptions(opt);
-    vbox->addWidget(_meshViewerWidget);
-
-    vbox->setContentsMargins(0, 0, 0, 0);
-
-    setLayout(vbox);
+    setCentralWidget(_meshViewerWidget);
 }
 
 
