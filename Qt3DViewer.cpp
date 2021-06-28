5c43feb1f0634f4b7a62454176ff300384bed017
pangkuanxin <351477478@qq.com>
2021-06-28 12:04:47 +0800

    增加Import Mesh action的创建

--- Qt3DViewer.cpp
+++ Qt3DViewer.cpp
@@ -1,9 +1,11 @@
 #include "Qt3DViewer.h"
 #include "openmeshApps/MeshViewerWidget.h"
+#include <qstyle.h>
 
 Qt3DViewer::Qt3DViewer(QWidget *parent)
     : QMainWindow(parent)
     , _meshViewerWidget(new MeshViewerWidget())
+    , _importMeshAction(new QAction(this))
 {
     ui.setupUi(this);
 
@@ -18,6 +20,10 @@ Qt3DViewer::Qt3DViewer(QWidget *parent)
 
     _meshViewerWidget->setOptions(opt);
     setCentralWidget(_meshViewerWidget);
+
+    createActions();
+    createMenus();
+    createToolbars();
 }
 
 
@@ -25,4 +31,27 @@ void Qt3DViewer::displayMesh()
 {
     OpenMesh::IO::Options opt;
     _meshViewerWidget->open_mesh("E:/skull.stl", opt);
+}
+
+void Qt3DViewer::createMenus()
+{
+    QMenu *fileMenu = this->menuBar()->addMenu(tr("File"));
+    fileMenu->addAction(_importMeshAction);
+}
+
+void Qt3DViewer::createToolbars()
+{
+    ui.mainToolBar->addAction(_importMeshAction);
+}
+
+void Qt3DViewer::createActions()
+{
+    _importMeshAction->setIcon(QIcon(":/Qt3DViewer/Resources/images/importMesh.png"));
+    _importMeshAction->setText(tr("Import Mesh"));
+    connect(_importMeshAction, &QAction::triggered, this, &Qt3DViewer::importMesh);
+}
+
+void Qt3DViewer::importMesh()
+{
+
 }
--- Qt3DViewer.h
+++ Qt3DViewer.h
@@ -4,18 +4,25 @@
 #include "ui_Qt3DViewer.h"
 
 class MeshViewerWidget;
-
+class QAction;
+class QToolBar;
 class Qt3DViewer : public QMainWindow
 {
     Q_OBJECT
 
 public:
     Qt3DViewer(QWidget *parent = Q_NULLPTR);
-
+private slots:
+    void importMesh();
 private:
     Ui::Qt3DViewerClass ui;
 
     MeshViewerWidget* _meshViewerWidget;
+    QAction* _importMeshAction;
+    QToolBar* _fileToolBar;
 
     void displayMesh();
+    void createMenus();
+    void createToolbars();
+    void createActions();
 };
